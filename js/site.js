24a6c31a5cf8a5a7cb14be7f95ab7e7e123ad623
Benedikt Werner <1benediktwerner@gmail.com>
2018-03-21 20:15:47 +0100

    Initial commit

---
+++ js/site.js
@@ -0,0 +1,27 @@
+﻿
+if (!sessionStorage.getItem("playedAnimation")) {
+    $("body").addClass("animation");
+    sessionStorage.setItem("playedAnimation", true);
+}
+
+// Write your Javascript code.
+$(function () {
+    $("#parallax1").click(() => $("body").toggleClass("parallax1"));
+    $("#parallax2").click(() => $("body").toggleClass("parallax2"));
+    $("#navbarLogo").click(() => $("body").toggleClass("navbarLogo"));
+});
+
+// Scroll buttons
+$(function () {
+    $('a.page-scroll').click(function (event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 86
+        }, 1500, "easeInOutExpo");
+        event.preventDefault();
+    });
+});
+
+// remove the focused state after click,
+// otherwise bootstrap will still highlight the link
+$("a").mouseup((e) => e.delegateTarget.blur());
