ac146ff0a1b8a6207e61dce514fab7d534680db9
Benedikt Werner <1benediktwerner@gmail.com>
2018-09-12 20:50:15 +0200

    All pages

--- copy.sh
+++ copy.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-wget localhost:5000
-mv index.html.1 index.html
+echo "Downloading index page"
+wget -q localhost:5000 -O index.html
+
+pages=( "Datenschutz" "Impressum" "Calendar" "News" "News/Test2" "News/Testartikel" "Teams" "Team/Würzburg" "Team/Regensburg" "Logo" )
+
+for page in "${pages[@]}"
+do
+	echo "Downloading ${page}"
+	mkdir -p ${page}
+	wget -q localhost:5000/${page} -O ${page}/index.html
+done
+
+echo "Copying static files"
 cp -r ../NewGetStrongWebsite/NewGetStrongWebsite/wwwroot/* ./
 
