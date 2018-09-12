ed4cb7da4c5527b147770e95212a9b60297d3547
Benedikt Werner <1benediktwerner@gmail.com>
2018-09-12 20:36:43 +0200

    Copy all static files

--- copy.sh
+++ copy.sh
@@ -2,7 +2,5 @@
 
 wget localhost:5000
 mv index.html.1 index.html
-cp ../NewGetStrongWebsite/NewGetStrongWebsite/wwwroot/css/* css/
-cp ../NewGetStrongWebsite/NewGetStrongWebsite/wwwroot/js/* js/
-cp -r ../NewGetStrongWebsite/NewGetStrongWebsite/wwwroot/lib/* lib/
+cp -r ../NewGetStrongWebsite/NewGetStrongWebsite/wwwroot/* ./
 
